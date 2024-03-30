b7d2c9e3de8948b971e04543f7d5f8c14fdb7584
nanderson castro <nandokstro@gmail.com>
2024-03-30 16:22:01 -0300

    Iniciando projeto com Laravel 11

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::middleware('auth')
+    ->prefix('/profile')
+    ->name('profile.')
+    ->controller(ProfileController::class)
+    ->group(function () {
+        Route::get('/', 'edit')->name('edit');
+        Route::patch('/', 'update')->name('update');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
